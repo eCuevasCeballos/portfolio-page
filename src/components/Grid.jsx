f76f4c6757f4b9d0f0b9d8f57f4dae029a796c22
erick <ecuevasceballos@gmail.com>
2023-07-20 19:13:36 -0600

    omnifoods addition

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,41 +1,58 @@
 /* Grid components */
-import React from 'react'
-import {BsGithub} from 'react-icons/bs'
-import {RiShareBoxLine} from 'react-icons/ri'
+import React from "react";
+import { BsGithub } from "react-icons/bs";
+import { RiShareBoxLine } from "react-icons/ri";
 
 /* Grid function takes 5 parameter */
-const Grid = ({ images, description,title, live, github }) => {
-
-
+const Grid = ({ images, description, title, live, github }) => {
   return (
-
     /* Main div */
-    <div className='grid grid-cols-1  xl:grid-cols-3 justify-items-center place-items-center gap-y-10 md:w-4/5 md:mx-auto'>
-      
+    <div className="grid grid-cols-1  xl:grid-cols-3 justify-items-center place-items-center gap-y-10 md:w-4/5 md:mx-auto">
       {
         /* Iterates over an array 2 parameters */
         images.map((image, index) => (
-        <div key={index} className='shadow-2xl rounded-lg overflow-hidden h-full md:w-4/5'>
+          <div
+            key={index}
+            className="shadow-2xl rounded-lg overflow-hidden h-full md:w-4/5"
+          >
+            {/* Gets the images */}
+            <img
+              src={image}
+              alt={`project${index} screenshot`}
+              className="w-full h-auto"
+            />
+            <h3 className="px-4 py-3 text-2xl font-semibold flex justify-center">
+              {title[index]}
+            </h3>
+            <p className="px-4">{description[index]}</p>
 
-          {/* Gets the images */}
-          <img src={image} alt={`project${index} screenshot`} className='w-full h-auto'/>
-          <h3 className='px-4 py-3 text-2xl font-semibold flex justify-center'>{title[index]}</h3>
-          <p className='px-4'>{description[index]}</p>
-        
-        {/* Links */}
-        <div className='flex justify-center items-center py-4 cursor-pointer font-medium'>
-          <RiShareBoxLine size={30} className='text-light-gray '/>
-          <a href={live[index]} target='_blank' rel='noopener noreferrer' className='text-1xl text-light-gray px-4 hover:text-blue hover:scale-110 duration-300'>See Live</a>
-          <h2 className='text-3xl pr-4'>|</h2>
-          <BsGithub size={30} className='text-light-gray'/>
-          <a href={github[index]} target='_blank' rel='noopener noreferrer' className='text-1xl text-light-gray px-4 hover:text-blue hover:scale-110 duration-300'>View Code</a>
-        </div>
-        </div>
+            {/* Links */}
+            <div className="flex justify-center items-center py-4 cursor-pointer font-medium">
+              <RiShareBoxLine size={30} className="text-light-gray " />
+              <a
+                href={live[index]}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-1xl text-light-gray px-4 hover:text-blue hover:scale-110 duration-300"
+              >
+                See Live
+              </a>
+              <h2 className="text-3xl pr-4">|</h2>
+              <BsGithub size={30} className="text-light-gray" />
+              <a
+                href={github[index]}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-1xl text-light-gray px-4 hover:text-blue hover:scale-110 duration-300"
+              >
+                View Code
+              </a>
+            </div>
+          </div>
         ))
-      } 
-
+      }
     </div>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,81 +1,113 @@
 /* Projects component */
-import React, { useState } from 'react'
-import {FaGripHorizontal} from 'react-icons/fa'
-import {BiCarousel} from 'react-icons/bi'
-import Carousel from './Carousel'
-import Grid from './Grid'
-import Project1 from '../assets/projects-1.png'
-import Project2 from '../assets/projects-2.png'
-import Project3 from '../assets/projects-3.png'
-import Project4 from '../assets/projects-4.png'
-import Project5 from '../assets/projects-5.png'
-import Project6 from '../assets/projects-6.png'
-import Project7 from '../assets/projects-7.png'
-import Description1 from '../assets/Descriptions/projects-1.json'
-import Description2 from '../assets/Descriptions/projects-2.json'
-import Description3 from '../assets/Descriptions/projects-3.json'
-import Description4 from '../assets/Descriptions/projects-4.json'
-import Description5 from '../assets/Descriptions/projects-5.json'
-import Description6 from '../assets/Descriptions/projects-6.json'
-import Description7 from '../assets/Descriptions/projects-7.json'
-
-
+import React, { useState } from "react";
+import { FaGripHorizontal } from "react-icons/fa";
+import { BiCarousel } from "react-icons/bi";
+import Carousel from "./Carousel";
+import Grid from "./Grid";
+import Project1 from "../assets/projects-1.png";
+import Project2 from "../assets/projects-2.png";
+import Project3 from "../assets/projects-3.png";
+import Project4 from "../assets/projects-4.png";
+import Project5 from "../assets/projects-5.png";
+import Project6 from "../assets/projects-6.png";
+import Project7 from "../assets/projects-7.png";
+import Project8 from "../assets/projects-8.png";
+import Description1 from "../assets/Descriptions/projects-1.json";
+import Description2 from "../assets/Descriptions/projects-2.json";
+import Description3 from "../assets/Descriptions/projects-3.json";
+import Description4 from "../assets/Descriptions/projects-4.json";
+import Description5 from "../assets/Descriptions/projects-5.json";
+import Description6 from "../assets/Descriptions/projects-6.json";
+import Description7 from "../assets/Descriptions/projects-7.json";
+import Description8 from "../assets/Descriptions/projects-8.json";
 
 const Projects = () => {
-
   /* Array */
   const projects = {
-
-    images: [Project1, Project2, Project3, Project4, Project5, Project6, Project7],
-    title : ['Social Proof Component', 'Order Summary Component', 'FAQ Component', 'Article Overview', 'E-sports Magazine', 'Interactive Rating Component', 'Rick & Morty api'],
-    description : [Description1, Description2, Description3, Description4, Description5, Description6, Description7],
-    live : [
-      'https://social-proof-section-ec.netlify.app/',
-      'https://order-summary-ec.netlify.app/',
-      'https://faq-accordion-card-ec.netlify.app/',
-      'https://article-preview-ec.netlify.app/',
-      'https://cosmo-techno.netlify.app/',
-      'https://interactive-rating-component-ec.netlify.app/',
-      'https://rick-morty-info-ec.netlify.app/'
+    images: [
+      Project1,
+      Project2,
+      Project3,
+      Project4,
+      Project5,
+      Project6,
+      Project7,
+      Project8,
     ],
-    github : [
-      'https://github.com/eCuevasCeballos/social-proof-section-master',
-      'https://github.com/eCuevasCeballos/order-summary-component-main',
-      'https://github.com/eCuevasCeballos/faq-accordion-card-main',
-      'https://github.com/eCuevasCeballos/article-preview-component-master',
-      'https://github.com/eCuevasCeballos/Proyecto_interfaces',
-      'https://github.com/eCuevasCeballos/interactive-rating-component-main',
-      'https://github.com/eCuevasCeballos/rick-and-morty'
-    ] 
-  }
-    
+    title: [
+      "Social Proof Component",
+      "Order Summary Component",
+      "FAQ Component",
+      "Article Overview",
+      "E-sports Magazine",
+      "Interactive Rating Component",
+      "Rick & Morty api",
+      "Omnifoosds Project",
+    ],
+    description: [
+      Description1,
+      Description2,
+      Description3,
+      Description4,
+      Description5,
+      Description6,
+      Description7,
+      Description8,
+    ],
+    live: [
+      "https://social-proof-section-ec.netlify.app/",
+      "https://order-summary-ec.netlify.app/",
+      "https://faq-accordion-card-ec.netlify.app/",
+      "https://article-preview-ec.netlify.app/",
+      "https://cosmo-techno.netlify.app/",
+      "https://interactive-rating-component-ec.netlify.app/",
+      "https://rick-morty-info-ec.netlify.app/",
+      "https://omnifood-p-erick-c.netlify.app/",
+    ],
+    github: [
+      "https://github.com/eCuevasCeballos/social-proof-section-master",
+      "https://github.com/eCuevasCeballos/order-summary-component-main",
+      "https://github.com/eCuevasCeballos/faq-accordion-card-main",
+      "https://github.com/eCuevasCeballos/article-preview-component-master",
+      "https://github.com/eCuevasCeballos/Proyecto_interfaces",
+      "https://github.com/eCuevasCeballos/interactive-rating-component-main",
+      "https://github.com/eCuevasCeballos/rick-and-morty",
+      "https://github.com/eCuevasCeballos/omnifoods-page",
+    ],
+  };
+
   /* Manages the icons Carousel & Grid */
-  const [icon, setIcon] = useState(false)
+  const [icon, setIcon] = useState(false);
 
   return (
-
     /* Main div */
-    <div className='w-full py-10 px-10 items-center' id='projects'>
-
+    <div className="w-full py-10 px-10 items-center" id="projects">
       {/* Section header + icons */}
-      <div className='flex justify-between md:w-3/5 md:mx-auto'>
-        <h3 className='text-black text-2xl font-bold  md:pl-0 md:text-3xl'><span className='text-blue font-icon font-semibold px-3' >&#60;&#47;&#62;</span>Projects</h3>
-        
+      <div className="flex justify-between md:w-3/5 md:mx-auto">
+        <h3 className="text-black text-2xl font-bold  md:pl-0 md:text-3xl">
+          <span className="text-blue font-icon font-semibold px-3">
+            &#60;&#47;&#62;
+          </span>
+          Projects
+        </h3>
+
         {/* Changes useState value on user click */}
-        <div onClick={() => (setIcon(!icon))} className='cursor-pointer flex items-center'>
-            <p className='pr-2'>Change view!</p>
-            {icon ? <FaGripHorizontal  size={30} /> : <BiCarousel size={30} />}
+        <div
+          onClick={() => setIcon(!icon)}
+          className="cursor-pointer flex items-center"
+        >
+          <p className="pr-2">Change view!</p>
+          {icon ? <FaGripHorizontal size={30} /> : <BiCarousel size={30} />}
         </div>
 
         {/* Displays the components using the icon */}
-        </div >
-          <div className='py-20 pb-0'>
-            {/* Adds the values to the parameters in the component */}
-            {icon ? <Carousel {...projects} /> : <Grid {...projects} />}
-        </div>
+      </div>
+      <div className="py-20 pb-0">
+        {/* Adds the values to the parameters in the component */}
+        {icon ? <Carousel {...projects} /> : <Grid {...projects} />}
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Projects
+export default Projects;
